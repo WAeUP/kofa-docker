1822e3b94c2cc1d174488d0d7f0e1854718c4902
ulif <uli@gnufix.de>
2016-02-15 11:10:26 +0100

    With the 2016 valentines day release we need less setup.
    The eases install remarkably. No extra versions of distribute,
    no multiple installs of setuptools required any more.

--- build.sh
+++ build.sh
@@ -3,11 +3,7 @@
 source /home/kofa/py27/bin/activate
 pip install --upgrade pip
 pip install pyopenssl ndg-httpsclient pyasn1
-pip install distribute
-pip install --upgrade setuptools==0.6c11
 
 cd waeup.kofa/
 python bootstrap.py
-pip install --upgrade zc.buildout==2.1.0
-pip install --upgrade setuptools==0.6c11
 ./bin/buildout
